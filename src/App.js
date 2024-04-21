5a49bfdbaad935e2435032f38ab711dfa98b09f8
sjb351 <115476852+sjb351@users.noreply.github.com>
2024-04-21 17:20:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Card, Row, Col, Dropdown, Button, Frame } from 'react-bootstrap';
+import { Card, Row, Col, Dropdown, Button } from 'react-bootstrap';
+
+
 const App = () => {
   const [selectedItem, setSelectedItem] = useState(null);
   const [error, setError] = useState(false);
@@ -10,109 +12,86 @@ const App = () => {
     setSelectedItem(item);
     setError(false);
   };
-  let url =  window.location.hostname 
 
- const websites = [
-    { name: [{name: "Dashboards (all)", link: 'http://' + url + ':3000/dashboards'},
-            {name: "Power Consumption", link: 'http://' + url + ":3000/d/q39y6tRgkV2/machine-dashboard-energy?orgId=1&refresh=5s&from=1713712676265&to=1713712976265&theme=light" },
-            {name: "Scrap Recorded", link: 'http://' + url + ":3000//d/YMtDdAX4k/overview?orgId=1&from=1713108106422&to=1713712906422&theme=light" },
-            {name: "Utilisation", link: 'http://' + url + ":3000/d/q39y6tRgk/machine-dashboard?orgId=1&refresh=5s&from=1713712384153&to=1713712684153" },
-            ]},
-    { name: "Database", link: 'http://' + url + ':8086' },
-    { name: "MES Sheduling", link: 'http://' + url + ':9000' },
-    { name: [{name: "Scrap Monitoring", link: 'http://' + url + ':7000'},
-            {name: "Scrap Monitoring Edit", link: 'http://' + url + ':7001/admin'},
-            ]},
-    { name: [
-        { name: "Tracking", link: '' },
-        { name: "Location Tracking", link: 'http://' + url},
-        { name: "Admin Location Tracking", link: 'http://' + url + ':8001' },
-        { name: "Composition Tracking", link: 'http://' + url + ':8001' },
-      ]
-    },
-    { name: [{name: "AAS Sending", link: 'http://' + url + ':6060'},
-            {name: "AAS Printing", link: 'http://' + url + ':6050'},
-            ]},
-    {name: "Workers", link: 'http://' + url + ':5050' },
-    {name: "Printer", link: 'http://' + url + ':5001' },
-  ];
+  let url = window.location.hostname;
+
+  // Assuming your websites data structure has been updated to be more consistent
+const websites = [
+  {
+    name: "Dashboards",
+    children: [
+      { name: "Dashboards (all)", link: `http://${url}:3000/dashboards` },
+      { name: "Power Consumption", link: `http://${url}:3000/d/q39y6tRgkV2/machine-dashboard-energy?orgId=1&refresh=5s&from=1713712676265&to=1713712976265&theme=light` },
+      { name: "Scrap Recorded", link: `http://${url}:3000//d/YMtDdAX4k/overview?orgId=1&from=1713108106422&to=1713712906422&theme=light` },
+      { name: "Utilisation", link: `http://${url}:3000/d/q39y6tRgk/machine-dashboard?orgId=1&refresh=5s&from=1713712384153&to=1713712684153` },
+    ]
+  },
+  { name: "Database", link: `http://${url}:8086` , children: null},
+  { name: "MES Scheduling", link: `http://${url}:9000` , children: null},
+  {
+    name: "Scrap Monitoring",
+    children: [
+      { name: "Scrap Monitoring", link: `http://${url}:7000` },
+      { name: "Scrap Monitoring Edit", link: `http://${url}:7001/admin` },
+    ]
+  },
+  {
+    name: "Tracking",
+    children: [
+      { name: "Location Tracking", link: `http://${url}`},
+      { name: "Admin Location Tracking", link: `http://${url}:8001`},
+      { name: "Composition Tracking", link: `http://${url}:8001`},
+    ]
+  },
+  {
+    name: "AAS",
+    children: [
+      { name: "AAS Sending", link: `http://${url}:6060` , children: null},
+      { name: "AAS Printing", link: `http://${url}:6050` , children: null},
+    ]
+  },
+  { name: "Workers", link: `http://${url}:5050`, children: null},
+  { name: "Printer", link: `http://${url}:5001`, children: null},
+];
+
 
   return (
-    <div>
-      <div style={{display: 'flex', justifyContent: 'space-between', padding: '6px', backgroundColor: '#6669ff', color:'white'}}>
-        <div key={1} style={{ cursor: 'pointer', marginRight: '20px' }}>VOOM Dashboard</div>
-      </div>
-      <div style={{display: 'flex', justifyContent: 'space-between', padding: '4px', backgroundColor: '#6669ff', color:'white'}}>
-        <div class="dropdown">
-          <button class="dropbtn">{websites[0].name[0].name}</button>
-          <div class="dropdown-content">
-            <a >
-              <div key={websites[0].name[0].name} onClick={() => handleItemClick(websites[0].name[0].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[0].name[0].name}</div>
-            </a>
-            <a>
-              <div key={websites[0].name[1].name} onClick={() => handleItemClick(websites[0].name[1].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[0].name[1].name}</div>
-            </a>
-            <a>
-              <div key={websites[0].name[2].name} onClick={() => handleItemClick(websites[0].name[2].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[0].name[2].name}</div>
-            </a>
-          </div>
-          </div>
-        <div key={websites[1].name} onClick={() => handleItemClick(websites[1].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[1].name}</div>
-        <div key={websites[2].name} onClick={() => handleItemClick(websites[2].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[2].name}</div>
-         <div class="dropdown">
-          <button class="dropbtn">{websites[3].name[0].name}</button>
-          <div class="dropdown-content">
-            <a >
-              <div key={websites[3].name[0].name} onClick={() => handleItemClick(websites[3].name[0].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[3].name[0].name}</div>
-            </a>
-            <a >
-              <div key={websites[3].name[1].name} onClick={() => handleItemClick(websites[3].name[1].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[3].name[1].name}</div>
-            </a>
-          </div>
-          </div>
-          <div class="dropdown">
-          <button class="dropbtn">{websites[4].name[0].name}</button>
-          <div class="dropdown-content">
-            <a >
-              <div key={websites[4].name[0].name} onClick={() => handleItemClick(websites[4].name[0].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[4].name[0].name}</div>
-            </a>
-            <a >
-              <div key={websites[4].name[1].name} onClick={() => handleItemClick(websites[4].name[1].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[4].name[1].name}</div>
-            </a>
-            <a >
-              <div key={websites[4].name[2].name} onClick={() => handleItemClick(websites[4].name[2].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[4].name[2].name}</div>
-            </a>
-            <a >
-              <div key={websites[4].name[3].name} onClick={() => handleItemClick(websites[4].name[3].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[4].name[3].name}</div>
-            </a>
-          </div>
-          </div>
-          <div class="dropdown">
-          <button class="dropbtn">{websites[5].name[0].name}</button>
-          <div class="dropdown-content">
-            <a >
-              <div key={websites[5].name[0].name} onClick={() => handleItemClick(websites[5].name[0].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[5].name[0].name}</div>
-            </a>
-            <a>
-              <div key={websites[5].name[1].name} onClick={() => handleItemClick(websites[5].name[1].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[5].name[1].name}</div>
-            </a>
-          </div>
-          </div>
-          <div key={websites[6].name} onClick={() => handleItemClick(websites[6].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[6].name}</div>
-          <div key={websites[7].name} onClick={() => handleItemClick(websites[7].link)} style={{ cursor: 'pointer', marginRight: '20px' }}>{websites[7].name}</div>
-      </div>
-     
+    <div className="app-container">
+      <div className="header">VOOM Dashboard</div>
+      <Row className="link-row">
+        {websites.map((website, index) =>
+          website.children ? (
+                <Dropdown as={Col} className="dropdown-col">
+                  <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                    {website.name}
+                  </Dropdown.Toggle>
 
-      <div style={{ marginTop: '5px' }}>
-        {error && <div style={{ color: 'red' }}>Website not found!</div>}
+                  <Dropdown.Menu>
+                    {website.children.map((child) => (
+                      <Dropdown.Item onClick={() => handleItemClick(child.link)} href={child.link}>{child.name}</Dropdown.Item>
+                    ))}
+                  </Dropdown.Menu>
+                </Dropdown>
+          ) : (
+            <Col key={index} className="link-col">
+              <Button variant="light" onClick={() => handleItemClick(website.link)}>
+                {website.name}
+              </Button>
+            </Col>
+          )
+        )}
+      </Row>
+
+      <div className="iframe-container">
+        {error && <div className="error">Website not found!</div>}
         {selectedItem && (
           <iframe
-            title={selectedItem}
+            title="content"
             src={selectedItem}
-            style={{ width: '100%', height: '800px' }}
+            width="100%"
+            height="800"
             onError={() => setError(true)}
-            frameborder="0"
-          /> 
+          />
         )}
       </div>
     </div>
